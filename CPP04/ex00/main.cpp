1abed8ada1e91d9243afdf3ca091c003117aa801
marineharutyunyan <m.marine.harutyunyan@gmail.com>
2023-07-03 11:39:01 +0400

    changed main

--- CPP04/ex00/main.cpp
+++ CPP04/ex00/main.cpp
@@ -7,25 +7,24 @@
 int main()
 {
 
-    // const Animal* meta = new Animal();
-    // const Animal* j = new Dog();
-    // const Animal* i = new Cat();
-    // std::cout << j->getType() << " " << std::endl;
-    // std::cout << i->getType() << " " << std::endl;
-    // i->makeSound();
-    // j->makeSound();
-    // meta->makeSound();
-
-    // delete meta;
-    // delete i;
-    // delete j;
-
-
-    const WrongAnimal* meta = new WrongAnimal();
-    const WrongAnimal* i = new WrongCat();
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
+    std::cout << j->getType() << " " << std::endl;
     std::cout << i->getType() << " " << std::endl;
     i->makeSound();
+    j->makeSound();
     meta->makeSound();
+
+    delete meta;
+    delete i;
+    delete j;
+
+    const WrongAnimal* meta1 = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+    std::cout << wrongCat->getType() << " " << std::endl;
+    wrongCat->makeSound();
+    meta1->makeSound();
 
 return 0;
 }
