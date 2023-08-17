968bb7cb88c02cf8449db531c270162586eb5845
marineharutyunyan <m.marine.harutyunyan@gmail.com>
2023-08-17 21:19:07 +0400

    CPP07 fixed according to checklist tests

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -2,6 +2,13 @@
 # define WHATEVER_HPP
 # include <iostream>
 
+template <typename T, typename P>
+void iter(T *array, size_t length, void (*func)(T&)) {
+    for (size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
+
 template <typename T>
 void iter(T *array, size_t length, void (*func)(T&)) {
     for (size_t i = 0; i < length; ++i) {
